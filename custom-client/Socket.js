d825767565e7ad1a220121d6b1872f7d51954ae9
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-03 03:54:45 +0700

    Update Socket.js

--- custom-client/Socket.js
+++ custom-client/Socket.js
@@ -1,1 +1,9 @@
-custom
+export class Socket {
+  static instance;
+  constructor() {
+      if (Socket.instance) return Socket.instance;
+      this.ws = new WebSocket(`wss://${window.location.host}/`);
+      Socket.instance = this;
+  }
+  
+}
