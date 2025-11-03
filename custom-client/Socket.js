f26a3f7d9dfea899bf61bea544f34a81d11caa35
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-03 16:16:25 +0700

    Update Socket.js

--- custom-client/Socket.js
+++ custom-client/Socket.js
@@ -11,7 +11,7 @@ export class Socket {
     Socket.instance = this;
   }
   #open () {
-    if (this.ws && this.ws.readyState === 0) return console.error('Error: Server is Connecting');
+    if (this.ws && (this.ws.readyState === 0 || this.ws.readySate === 1)) return console.error('Error: Server is already');
     try {
       this.ws = new WebSocket(`wss://${location.host}/`);
       this.ws.onopen = () => console.log('Connected!');
@@ -19,37 +19,38 @@ export class Socket {
       this.ws.onerror = (e) => console.error(`Error: ${e.target}`);
       this.ws.onclose = (e) => console.log(`Closed: [${e.code}] ${e.reason}`);
     } catch {
-      console.error('Error: Not Connect to Server');
+      console.error('Error: Not connect to Server');
     } finally {
       console.info('Server Connecting...');
     }
   }
   #clean () {
-    if (!this.ws) return console.error('Not Clean');
+    if (!this.ws) return console.error('Not clean');
     this.ws.onopen = null;
     this.ws.onmessage = null;
     this.ws.onerror = null;
     this.ws.onclose = null;
-    this.ws.close();
     this.ws = null;
   }
   connect() {
-    if (this.ws && this.ws.readyState === 1) return console.error('Error: Server is running');
-    this.#clean();
+    if (this.ws) return console.error('Error: WebSocket is open');
+    if (this.ws.readState === 0 || this.ws.readyState === 1) return console.error('Error: Server is running');
     this.#open();
   }
   send(data) {
-    if (!this.ws && this.ws.readyState !== 1) return console.error('Error: Cannot Send');
+    if (!this.ws || this.ws.readyState !== 1) return console.error('Error: Cannot send');
     this.ws.send(data);
   }
-  on() {
+  on(callback) {
     if (!this.ws) return console.error('Error: Cannot onMessage');
     this.ws.addEventListener("message", (e) => {
       console.log('Revice: ', e.data);
+      if (callback) callback(e.data);
     });
   }
   close(code = 1000, message = "Server closed") {
-    if (!this.ws) return console.error('Error: Cannot Close');
+    if (!this.ws) return console.error('Error: Cannot close');
+    this.ws.close(code, message);
     this.#clean();
     console.log(`Closed: [${code}] ${message}`);
 }
