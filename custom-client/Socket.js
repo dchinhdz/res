10117030d59b1ce8e49cad69cd15032c92466010
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-03 14:10:15 +0700

    Update Socket.js

--- custom-client/Socket.js
+++ custom-client/Socket.js
@@ -1,9 +1,21 @@
 export class Socket {
   static instance;
   constructor() {
-      if (Socket.instance) return Socket.instance;
-      this.ws = new WebSocket(`wss://${window.location.host}/`);
-      Socket.instance = this;
+    if (Socket.instance) return Socket.instance;
+    this.#open();
+    Socket.instance = this;
   }
-  
+  #open () {
+    try {
+      this.ws = new WebSocket(`wss://${location.host}/`);
+      this.ws.onopen = () => console.log('Connected!');
+      this.ws.onmessage = (e) => console.log(e.message); //welcome to...
+      this.ws.onerror = (e) => console.error(`Error: ${e.target}`);
+      this.ws.onclose = (e) => console.log(`Closed: ${e.code} - ${e.reason}`);
+    } catch {
+    console.error('Error: Not Connect to Server');
+    } finally {
+      console.info('Server Connecting...');
+    }
+  }
 }
