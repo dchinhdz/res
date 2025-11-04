117bd5689b841df974a3a428d65a550d44515986
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-04 17:32:30 +0700

    Update Socket.js

--- custom-client/Socket.js
+++ custom-client/Socket.js
@@ -1,17 +1,20 @@
 export class Socket {
   constructor(url) {
     this.ws = null;
+    this.delay = 1000;
     this.url = url || `wss://${location.host}/`;
-    this.#open;
+    this.#open();
   }
   #open () {
     if (this.ws) return;
     this.ws = new WebSocket(this.url);
   }
   #clean () {
-    try {
-      this.ws.close();
-    } catch {
-      this.ws.onopen = this.ws.onmessage = this.ws.onerror = this.onclose = null;
-    }
+    this.ws.onopen = this.ws.onmessage = this.ws.onerror = this.ws.onclose = null;
+    this.ws = this.ws && (this.ws.close(), null);
+    this.delay = 1000;
+  }
+  autoconnect() {
+    if (this.ws.readyState < 2) return;
+    
 }
