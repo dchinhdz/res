37d99976328f267e59b083ce203a8f91d9151e0e
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-04 18:01:20 +0700

    Update Socket.js

--- custom-client/Socket.js
+++ custom-client/Socket.js
@@ -7,14 +7,28 @@ export class Socket {
   }
   #open () {
     if (this.ws) return;
+    this.#clean();
     this.ws = new WebSocket(this.url);
+    this.ws.binaryType = "arraybuffer";
   }
   #clean () {
     this.ws.onopen = this.ws.onmessage = this.ws.onerror = this.ws.onclose = null;
     this.ws = this.ws && (this.ws.close(), null);
-    this.delay = 1000;
   }
-  autoconnect() {
-    if (this.ws.readyState < 2) return;
     
+  autoconnect() {
+    const auto = setInterval(() => {
+      if (this.ws.readyState < 2) {
+        clearInterval(auto);
+        this.delay = 1000;
+      } else {
+        this.#open();
+        this.delay = this.delay <== 30000 ? this.delay * 2 : 30000;
+      }
+    }, this.delay);
+  }
+  close() {
+    if (this.ws.readyState > 1) return;
+    this.#clean();
+  }
 }
