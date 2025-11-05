4a7a11e060c387d3cea9cdcd2b264c374c21202d
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-05 20:39:54 +0700

    Update Emits.js

--- custom-client/Emits.js
+++ custom-client/Emits.js
@@ -1,19 +1,32 @@
 export class Emits {
-  constructor(ws) {
-    this.ws = ws;
+  constructor(socket) {
+    this.ws = socket;
   }
   _buffer(c, n) {
-    const buffer = new ArrayBuffer(n);
+    const buffer = new ArrayBuffer(n+1);
     const q = new DataView(buffer);
     q.setUint8(0, c);//channel
     return q;
   }
   uMove(x, y) {
-    const q = this._buffer(1,9);
+    const q = this._buffer(1,8);
     q.setFloat32(1, x, true);
     q.setFloat32(5, y, true);
     return q.buffer;
   }
+  uAttack(id) {
+    return Number(id);
+  }
+  mBuy(id) {
+    return Number(id);
+  }
+  mSell(id) {
+    return Number(id);
+  }
+  mDrop(id) {
+    return Number(id);
+  }
+  
   emit(...args) {
     this.ws.emit(...args);
   }
