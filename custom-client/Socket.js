15e531b673a82a63749bbeaa77bafb286196eed1
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-05 18:59:35 +0700

    Update Socket.js

--- custom-client/Socket.js
+++ custom-client/Socket.js
@@ -1,32 +1,35 @@
+// Socket.js
 export class Socket {
   constructor(url) {
-    this.u = url || `wss://${location.host}/`;
-    this.d = 1000;
-    this.h = { o: [], c: [], m: [], e: [] };
+    this.url = url || `wss://${location.host}/`;
+    this.delay = 1000;
+    this.handlers = { open: [], close: [], message: [], error: [] };
     this.#connect();
   }
 
   #connect() {
     if (this.ws?.readyState < 2) return;
     this.#cleanup();
-    this.ws = new WebSocket(this.u);
+
+    this.ws = new WebSocket(this.url);
     this.ws.binaryType = "arraybuffer";
+
     this.ws.onopen = () => {
-      this.d = 1000;
-      this.h.o.forEach(h => h());
+      this.delay = 1000;
+      this.handlers.open.forEach(h => h());
     };
     this.ws.onclose = () => {
-      this.h.c.forEach(h => h());
+      this.handlers.close.forEach(h => h());
       this.#reconnect();
     };
-    this.ws.onmessage = e => this.h.m.forEach(h => h(e));
-    this.ws.onerror = e => this.h.e.forEach(h => h(e));
+    this.ws.onmessage = e => this.handlers.message.forEach(h => h(e));
+    this.ws.onerror = e => this.handlers.error.forEach(h => h(e));
   }
 
   #cleanup() {
     if (this.ws) {
       this.ws.onopen = this.ws.onclose = this.ws.onmessage = this.ws.onerror = null;
-      if (this.ws.readyState < 2) this.ws.close();
+      if (this.ws.readyState === 1) this.ws.close();
       this.ws = null;
     }
   }
@@ -34,16 +37,16 @@ export class Socket {
   #reconnect() {
     setTimeout(() => {
       this.#connect();
-      this.d = Math.min(this.d * 2, 10000);
-    }, this.d);
+      this.delay = Math.min(this.delay * 2, 10000);
+    }, this.delay);
   }
 
-  on(e, c) {
-    if (this.h[e]) this.h[e].push(c);
+  on(event, cb) {
+    if (this.handlers[event]) this.handlers[event].push(cb);
   }
 
-  emit(e) {
-    this.ws?.readyState === 1 && this.ws.send(e);
+  emit(data) {
+    this.ws?.readyState === 1 && this.ws.send(data);
   }
 
   close() {
