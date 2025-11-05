34aa798370101aa51648b44043ca1b131babb5eb
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-05 21:02:38 +0700

    Fix uAct method to handle undefined user actions

--- custom-client/Emits.js
+++ custom-client/Emits.js
@@ -1,6 +1,7 @@
 export class Emits {
   constructor(socket) {
     this.ws = socket;
+    this.u = {move: 1, attack: 2, buy: 3, sell: 4, drop: 5};
   }
   _buffer(c, n) {
     const buffer = new ArrayBuffer(n+1);
@@ -9,24 +10,17 @@ export class Emits {
     return q;
   }
   uMove(x, y) {
-    const q = this._buffer(1,8);
+    const q = this._buffer(this.u.move,8);
     q.setFloat32(1, x, true);
     q.setFloat32(5, y, true);
     return q.buffer;
   }
-  uAttack(id) {
-    return Number(id);
-  }
-  mBuy(id) {
-    return Number(id);
-  }
-  mSell(id) {
-    return Number(id);
-  }
-  mDrop(id) {
-    return Number(id);
-  }
-  
+  uAct(c, id) {
+    if (!this.u[c]) return;
+    const q = this._buffer(this.u[c], 2);
+    q.setUint16(1, id, true);
+    return q.buffer;
+  } 
   emit(...args) {
     this.ws.emit(...args);
   }
