27f904c1940b3774b9f8a5f04a1a7002f361c74e
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-06 00:39:36 +0700

    Update Emits.js

--- custom-client/Emits.js
+++ custom-client/Emits.js
@@ -1,7 +1,8 @@
 export class Emits {
   constructor(socket) {
     this.ws = socket;
-    this.u = {move: 1, attack: 2, buy: 3, sell: 4, drop: 5};
+    this.channel = {attack: 1, buy: 2, sell: 3, drop: 4, use: 5};
+    this.map = {g: 1};
   }
   _buffer(c, n) {
     const buffer = new ArrayBuffer(n+1);
@@ -9,15 +10,17 @@ export class Emits {
     q.setUint8(0, c);//channel
     return q;
   }
-  uMove(x, y) {
-    const q = this._buffer(this.u.move,8);
-    q.setFloat32(1, x, true);
-    q.setFloat32(5, y, true);
+  uMove(id, x, y) {
+    if (!this.map[id]) return;
+    const q = this._buffer(0,9);
+    q.setUint8(1, this.map[id]);
+    q.setFloat32(2, x, true);
+    q.setFloat32(6, y, true);
     return q.buffer;
   }
-  uAct(c, id) {
-    if (!this.u[c]) return;
-    const q = this._buffer(this.u[c], 2);
+  uAtc(c, id) {
+    if (!this.channel[c]) return;
+    const q = this._buffer(this.channel[c], 2);
     q.setUint16(1, id, true);
     return q.buffer;
   } 
