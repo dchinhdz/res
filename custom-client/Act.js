cf2b5f11f24934e235081582830b4f6ff06686d3
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-06 14:50:08 +0700

    Update and rename Emits.js to Act.js

--- custom-client/Act.js
+++ custom-client/Act.js
@@ -1,26 +1,20 @@
-export class Emits {
-  constructor(socket) {
-    this.ws = socket;
-  }
+export class Act {
   _buffer(c, n) {
     const buffer = new ArrayBuffer(n+1);
     const q = new DataView(buffer);
     q.setUint8(0, c);//channel
     return q;
   }
-  uMove(map, x, y) {
-    const q = this._buffer(0,9);
-    q.setUint8(1, map);
+  move(map, x, y) {
+    const q = this._buffer(1,9);
+    q.setUint8(1, map);//id map
     q.setFloat32(2, x, true);
     q.setFloat32(6, y, true);
     return q.buffer;
   }
-  uAtc(c, id) {
+  cmd(c, id) {
     const q = this._buffer(c, 2);
-    q.setUint16(1, id, true);
+    q.setUint16(1, id, true);//id
     return q.buffer;
-  } 
-  emit(...args) {
-    this.ws.emit(...args);
   }
 }
