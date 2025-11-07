71106becbcdcc6114b1dc15168cc66fa8f7cbbf2
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-08 00:43:52 +0700

    Create F.js

---
+++ custom-client/F.js
@@ -0,0 +1,19 @@
+export class F {
+  static async p(f) {
+    try {
+     const r = await fetch('/', {
+       method: 'post',
+       headers: {'Content-Type': 'application/json'},
+       body: JSON.stringify(f),
+       credentials: 'include'
+     });
+      return r.ok ? (await r.json().catch(() => false)) : false;
+    } catch {return false};
+  }
+  static async g(f) {
+    try {
+      const r = await fetch(`/${f}`, {method: 'get'});
+      return r.ok ? (await r.json().catch(() => false)) : false;
+    } catch {return false};
+  }
+}
