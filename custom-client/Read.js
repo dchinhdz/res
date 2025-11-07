c60e56f563d9cae325e0c4b4e6b4a5fa6a23908a
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-07 17:17:33 +0700

    Update Read.js

--- custom-client/Read.js
+++ custom-client/Read.js
@@ -1,22 +1,19 @@
 export class Read {
   static channel(data) {
-    if (Array.isArray(data)) return this.data[0];
-    return this.data?.channel ?? null;
+    if (Array.isArray(data)) return data[0];
+    return data?.channel ?? null;
   }
   static userId(data) {
-    if (Array.isArray(data)) return this.data[1];
-    return this.data?.channel ?? null;
+    if (Array.isArray(data)) return data[1];
+    return data?.userId ?? null;
   }
   static move(data) {
-    if (Array.isArray(data)) {
     return {userId: data[1],  map: data[2], x: data[3], y: data[4]};
-    }
-    return null;
   }
   static str(data) {
-    if (Array.isArray(data)) return data[2] ?? null;
+    return data[2] ?? null;
   }
   static cmd(data) {
-    if (Array.isArray(data)) return data[2] ?? null;
+    return data[2] ?? null;
   }
 }
