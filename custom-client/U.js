de744e1a4715431747e297fd7b6360aa23b0a217
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-08 01:03:49 +0700

    Create U.js

---
+++ custom-client/U.js
@@ -0,0 +1,7 @@
+export class U {
+  static u = (u) => /^[a-zA-Z0-9]{4,20}$/.test(String(u));
+  static p = (p) => /^.{6,16}$/.test(String(p));
+  static e = (e) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(String(e).toLowerCase());
+  static P = (p) => /^(?:\+84|0)(?:3|5|7|8|9)\d{8}$/.test(String(phone));
+  
+}
