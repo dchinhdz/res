17d284fc156f65395767113ff74d7921d32b1bce
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-08 01:52:26 +0700

    Update App.js

--- custom-client/App.js
+++ custom-client/App.js
@@ -1,3 +1,24 @@
 import {S} from './S.js';
+import {N} from './N.js';
 class App {
-  static 
+  constructor() {
+    this.u = "wss://websocket-echo.com";
+    this._i();
+  }
+  _i() {
+    try {
+      this.s = new S(this.u);
+      this.s?.o('o', () => N.s('Connected!'));
+      this.s?.o('e', () => N.s('Error!'));
+      this.s?.o('c', () => N.s('Disconnect!'));
+      this.s?.e('Hello Server!');
+    } catch {
+      N.s('Not open Websocket!');
+    }
+  }
+  run() {
+    if (this.s && this.s.s() !== 1) return N.s('Websocket chưa sẵn sàng');
+    this.s?.o('m', (e) => N.s(e.data));
+  }
+}
+const App = new App().run();
