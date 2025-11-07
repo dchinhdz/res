27144c22979d57a618bb127571702d921d0fb3f4
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-08 02:31:13 +0700

    Update App.js

--- custom-client/App.js
+++ custom-client/App.js
@@ -8,17 +8,13 @@ class App {
   _i() {
     try {
       this.s = new S(this.u);
-      this.s?.o('o', () => N.s('Connected!'));
-      this.s?.o('e', () => N.s('Error!'));
-      this.s?.o('c', () => N.s('Disconnect!'));
-      this.s?.e('Hello Server!');
+      this.s.o('m', () => (e) => N.s(e.data));
+      this.s.o('o', () => N.s('Connected!'));
+      this.s.o('e', () => N.s('Error!'));
+      this.s.o('c', () => N.s('Disconnect!'));
     } catch {
       N.s('Not open Websocket!');
     }
   }
-  run() {
-    if (this.s && this.s.s() !== 1) return N.s('Websocket chưa sẵn sàng');
-    this.s?.o('m', (e) => N.s(e.data));
-  }
 }
-const app = new App().run();
+const app = new App();
