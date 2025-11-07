697f396e5443e070120daac83a638d7ece26f022
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-07 23:54:56 +0700

    Create L.js

---
+++ custom-client/L.js
@@ -0,0 +1,6 @@
+export class L {
+  static s = (k, v) => localStorage.setItem(k, JSON.stringify(v));
+  static g = (k) => JSON.parse(localStorage.getItem(k) ?? null) ?? null;
+  static r = (k) => localStorage.removeItem(k);
+  static c = () => localStorage.clear();
+}
