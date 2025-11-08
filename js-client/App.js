0564af00869e89551e32ebed1e8a30a95ab2be29
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-08 18:20:38 +0700

    Update App.js

--- js-client/App.js
+++ js-client/App.js
@@ -12,21 +12,23 @@ class App {
       this.s.o('m', (e) => this._run(e.data));
       this.s.o('o', () => D.n('Connected!'));
       this.s.o('e', () => D.n('Error!'));
-      this.s.o('c', () => D.n('Disconnect!'));
+      this.s.o('c', () => this._dis());
     } catch {
       D.n('Not open Websocket!');
     }
   }
   ping() {
-    if (this.s?.s !== 1) return; 
     this.p = performance.now();
-    this.s.e(A.p());
+    this.s?.e(A.p());
   }
   _run(e) {
-    if (this.s?.s === 1 && A.iB(e) && A.iP(e)) return D.p(Math.round(performance.now() - this.p));
-    if (this.s?.s !== 1) return D.p(999);
+    if (A.iB(e) && A.iP(e)) return D.p(Math.round(performance.now() - this.p));
     D.n(e);
   }
+  _dis() {
+    D.n('Disconnect!');
+    D.q(999);
+}
 }
 const app = new App();
 setInterval(app.ping.bind(app), 5000);
