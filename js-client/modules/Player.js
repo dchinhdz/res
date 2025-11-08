76c7fe542811fba70ccbb17935c2beaee7201baf
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-09 01:12:55 +0700

    Create Player.js

---
+++ js-client/modules/Player.js
@@ -0,0 +1,21 @@
+export class P {
+  constructor(arr) {
+    this.item = arr || [];
+    this.demo();
+  }
+  demo() {
+    this.item.push({[10710],41,-18});//bieutuong
+    this.item.push({[10728],-15,45});//haoquang
+    this.item.push({[5900],-28,0});//canh
+    this.item.push({[201],9,46});//quan
+    this.item.push({[193],5,34});//ao
+    this.item.push({[002],0,0});//khuanmat
+    this.item.push({[174],12,15});//mat
+    this.item.push({[9351],-5,-16});//toc
+    this.item.push({[0],0,0});//kinh
+    this.item.push({[155],21,25});//matna
+    this.item.push({[0],0,0});//non
+    this.item.push({[115],30,34});//docamtay
+    this.item.push({[10725],-86,0});//pet
+  }
+}
