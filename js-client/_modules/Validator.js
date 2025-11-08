021c53205b996a8686964dd47ac02edfede136a0
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-08 16:28:46 +0700

    Create Validator.js

---
+++ js-client/_modules/Validator.js
@@ -0,0 +1,8 @@
+export class V {
+  static u = (u) => /^[a-zA-Z0-9]{4,20}$/.test(String(u));
+  static p = (p) => /^.{6,16}$/.test(String(p));
+  static e = (e) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(String(e).toLowerCase());
+  static P = (p) => /^(?:\+84|0)(?:3|5|7|8|9)\d{8}$/.test(String(p));
+  static c = (c) => /^.{1,200}$/.test(String(c));
+  static s = (a, b) => a && b && (a === b);
+}
