e7379b652488c2d2f8a6a5a0886dd6044682fd8e
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-09 13:23:35 +0700

    Update Player.js

--- js-client/modules/Player.js
+++ js-client/modules/Player.js
@@ -9,7 +9,7 @@ export class P {
     this.item.push([["10710"],41,-18]);//bieutuong
     this.item.push([["10728"],-15,45]);//haoquang
     this.item.push([["5900"],-28,0]);//canh
-    this.item.push([["201"],9,46]);//quan
+    this.item.push([["201", "202"],9,46]);//quan
     this.item.push([["193"],5,34]);//ao
     this.item.push([["002"],0,0]);//khuanmat
     this.item.push([["174"],12,15]);//mat
@@ -18,7 +18,7 @@ export class P {
     this.item.push([["155"],21,25]);//matna
     this.item.push([["0"],0,0]);//non
     this.item.push([["115"],30,34]);//docamtay
-    this.item.push([["10725"],-86,0]);//pet
+    this.item.push([["10725", "10726"],-86,0]);//pet
   }
   //f = (i) => Object.assign(new Image(), { src: `/img/item/hd/${i}.png` });
   f(i) {
@@ -31,15 +31,19 @@ export class P {
   }
   
   async c() {
-    const j = this.item.flatMap(k => k[0]);
-    const l = await Promise.all(j.map(i => this.f(i)));
-    l.forEach((i, k) => {
-      if (!i) return;
-      this.c.mW = Math.max(this.c.mW, i.naturalWidth);
-      this.c.mH = Math.max(this.c.mH, i.naturalHeight);
-      if (j[k] === this.root) {
-        this.c.rW = i.naturalWidth;
-        this.c.rH = i.naturalHeight;
+    const j = this.item.flatMap((a, k) => a[0].map(v => ({k, v, x:a[1], y:a[2]})));
+    const k = j.map(k => k.k);//key
+    const v = j.map(v => v.v);//value
+    const x = j.map(x => x.x);//x
+    const y = j.map(y => y.y);//y
+    const l = await Promise.all(v.map(i => this.f(i)));//value map
+    l.forEach((h, i) => {
+      if (!h) return;
+      this.c.mW = Math.max(this.c.mW, h.naturalWidth);
+      this.c.mH = Math.max(this.c.mH, h.naturalHeight);
+      if (v[i] === this.root) {
+        this.c.rW = h.naturalWidth;
+        this.c.rH = h.naturalHeight;
       }
     });
   }
