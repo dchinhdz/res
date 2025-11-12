a24ea79e4a1a141599d4b8d5cfdea831e54a37a1
dchinhdz <70805176+dchinhdz@users.noreply.github.com>
2025-11-12 14:27:50 +0700

    Update Players.js

--- js-client/modules/Players.js
+++ js-client/modules/Players.js
@@ -1,9 +1,22 @@
 export class P {
   static data = {};
+  static cacheImage = {};
   static start(d) {
     if (d === null || typeof d !== "object" || Array.isArray(d)) return this.data;
-    if (d.
+    if (!Object.keys(d).length || !d.map || !d.player) return this.data;
     this.data = d;
     return true;
+  }
+  static update(d) {
+    if (d === null || typeof d !== "object" || !d.player) return null;
+    this.data = Object.assign(this.data, d);
+  }
+  addPlayer() {
+  }
+  removePlayer() {
+  }
+  movePlayer() {
+  }
+  drawCanvas() {
   }
 }
